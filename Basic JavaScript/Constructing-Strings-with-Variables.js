967cbcac44afd2db359ce5d802a01ae3b940d0fe
ankithdadda <ankithdadda@gmail.com>
2024-11-16 17:21:35 +0530

    JS day 2

---
+++ Basic JavaScript/Constructing-Strings-with-Variables.js
@@ -0,0 +1,3 @@
+// Only change code below this line
+const myName = "Ankith";
+const myStr = "My name is "+myName+" I am well!";
---
+++ Basic JavaScript/Nest-One-Array-within-Another-Array.js
@@ -0,0 +1,2 @@
+// Only change code below this line
+const myArray = [["Dinesh", 56], ["Ankith", 21]];
