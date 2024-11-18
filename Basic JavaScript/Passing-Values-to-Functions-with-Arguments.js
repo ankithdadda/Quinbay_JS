17468352efc56d466a0add75d5924985288ad8c5
ankithdadda <ankithdadda@gmail.com>
2024-11-18 19:39:24 +0530

    JS day 3

---
+++ Basic JavaScript/Passing-Values-to-Functions-with-Arguments.js
@@ -0,0 +1,6 @@
+function functionWithArgs(a1, a2){
+    var sum = a1+a2;
+    console.log(sum);
+  }
+  
+  functionWithArgs(5,20);
---
+++ Basic JavaScript/Shopping-List.js
@@ -0,0 +1,6 @@
+const myList = [];
+myList.push(["Chocolate Bar", 15])
+myList.push(["Peanut butter", 25])
+myList.push(["Cookies", 35])
+myList.push(["Noodles", 45])
+myList.push(["Red Bull", 50])
