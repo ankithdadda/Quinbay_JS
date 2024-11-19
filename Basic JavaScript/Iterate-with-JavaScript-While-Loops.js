a318592c2fb4c0c2e9744fb2482fe3cbd285ecc1
ankithdadda <ankithdadda@gmail.com>
2024-11-19 23:19:45 +0530

    JS day 5

---
+++ Basic JavaScript/Iterate-with-JavaScript-While-Loops.js
@@ -0,0 +1,9 @@
+// Setup
+const myArray = [];
+var i = 5;
+while (i > -1){
+  myArray.push(i);
+  i--;
+}
+
+// Only change code below this line
---
+++ Basic JavaScript/Manipulating-Complex-Objects.js
@@ -0,0 +1,23 @@
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    }
+  ];
+  
+  myMusic.push({
+    "artist": "Anuv Jain",
+    "title": "Gul",
+    "release_year": 2021,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"]
+  });
