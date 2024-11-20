1663e17767fb6ade40570e146f8b32fd6925f974
ankithdadda <ankithdadda@gmail.com>
2024-11-20 23:30:35 +0530

    JS day 6

---
+++ Basic JavaScript/Use-Recursion-to-Create-a-Range-of-Numbers.js
@@ -0,0 +1,11 @@
+function rangeOfNumbers(startNum, endNum) {
+    if (startNum === endNum){
+      return [startNum];
+    }
+  
+    else{
+      const output = rangeOfNumbers(startNum + 1, endNum);
+      output.unshift(startNum);
+      return output;
+    }
+  };
